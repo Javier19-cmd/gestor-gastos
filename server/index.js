7cd5a344aed80bedcf1ab02cc963d944a5c9238e
Javier Sebastián Valle <63107636+Javier19-cmd@users.noreply.github.com>
2024-08-19 20:52:26 -0600

    security updates

--- server/index.js
+++ server/index.js
@@ -1,28 +1,52 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const helmet = require('helmet');
 require('dotenv').config();
 const app = express();
 
-app.use(cors());
+// Configuración de CORS más segura
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+app.use(cors(corsOptions));
+
+// Eliminando encabezado X-Powered-By
+app.disable('x-powered-by');
+
+// Añadiebdi encabezados de seguridad con helmet
+app.use(helmet());
+app.use(helmet.hsts({
+  maxAge: 63072000,
+  includeSubDomains: true,
+  preload: true
+}));
+app.use(helmet.noSniff());
+
+// Configuración de directivas de caché
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
+  res.setHeader('Pragma', 'no-cache');
+  next();
+});
+
 app.use(express.json());
 
-// Rutas de autenticación
+// Rutas
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
-// Rutas de transacciones
 const transactionRoutes = require('./routes/transactions');
 app.use('/api/transactions', transactionRoutes);
 
-// Rutas de estadísticas
 const statsRoutes = require('./routes/stats');
 app.use('/api/stats', statsRoutes);
 
-// Rutas de usuarios
 const userRoutes = require('./routes/user');
 app.use('/api/user', userRoutes);
-
 
 // Ruta por defecto
 app.use("/", (req, res) => {
